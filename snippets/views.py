8f6c61beee6d97aac360b40dfa5ccda98c6a9c11
aniket <aniket14goyal@gmail.com>
2018-03-29 04:29:08 +0530

    commit to save data

--- snippets/views.py
+++ snippets/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from snippets.forms import SnippetForm
+from snippets.models import Snippet
+def create(request) :
+  if request.method == "GET":
+    form=SnippetForm()
+  elif request.method == "POST":
+    form=SnippetForm(request.POST)
+    if form.is_valid() :
+      data=form.cleaned_data
+      snippet=Snippet(title=data.get('title'),content=data.get('content'))
+      snippet.save()
+      return redirect('view',snippet.id)
+  return render(request,'snippets/create.html',{'form':form})
 
-# Create your views here.
+def view(request,key):
+  return HttpResponse("View function"+str(key))
